7b502d0d84a125bf25582d7d12088956d692207b
Kswartz11 <70081928+Kswartz11@users.noreply.github.com>
2021-07-16 19:18:45 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 # import dependencies
-from models import create_classes
+# from models import create_classes
 import os
-import numpy as np
-import pickle #Initialize the flask App
-from flask import Flask, render_template, jsonify, request, redirect
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-import statsmodels.api as sm
-from sklearn import linear_model
+# import numpy as np
+# import pickle #Initialize the flask App
+# from flask import Flask, render_template, jsonify, request, redirect
+# from sklearn import preprocessing
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn import metrics
+# from sklearn.metrics import confusion_matrix
+# import statsmodels.api as sm
+# from sklearn import linear_model
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
 
 #################################################
 # Database Setup
@@ -41,28 +41,28 @@ def home():
 
 
 # Render prediction
-@app.route("/predict", methods=["GET", "POST"])
+# @app.route("/predict", methods=["GET", "POST"])
 
-def predict():
-    #For rendering results on HTML GUI
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    output = round(prediction[0], 2) 
+# def predict():
+#     #For rendering results on HTML GUI
+#     int_features = [float(x) for x in request.form.values()]
+#     final_features = [np.array(int_features)]
+#     prediction = model.predict(final_features)
+#     output = round(prediction[0], 2) 
 
-    return render_template('index.html', prediction_text='Liklihood of loan default is :{}'.format(output))
+#     return render_template('index.html', prediction_text='Liklihood of loan default is :{}'.format(output))
 
 
 #Render results
-@app.route('/results',methods=['POST'])
+# @app.route('/results',methods=['POST'])
 
-def results():
+# def results():
 
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+#     output = prediction[0]
+#     return jsonify(output)
 
 
 if __name__ == "__main__":
